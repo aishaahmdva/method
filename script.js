ee039db0635cfb6873b5e126d4b7f1cf958af28f
aisha <axmdvaisha@gmail.com>
2023-07-04 20:58:18 +0400

    dusturlar

---
+++ script.js
@@ -0,0 +1,44 @@
+// let num =alert('Aisha cox savadli agillidir')
+// let result = confirm('yasiniz 18 den yuxaridiremi')
+
+// let mesafe  = +prompt('mesafeni qeyd edin: ');
+// let suret = +prompt('Hansi suretle hereket edirsiniz?: ');
+
+// let zaman = mesafe / suret;
+// if(result){
+//     result ='Sen b u kinoya baxa bilersen';
+
+// }else{
+//     result ='Sen usaqsan get dersini oxu';
+// }
+// document.getElementById('aspa').innerHTML = zaman;
+
+
+
+
+
+let reqem  = +prompt('reqemleri qeyd edin: ');
+let sayi = +prompt('ededi orta tapmaq ucun sayina bolecik: ');
+
+let edediOrta = reqem / sayi;
+document.getElementById('aspa').innerHTML =edediOrta;
+
+
+
+
+
+let a  = +prompt('terefleri ');
+let b = +prompt('sahesini tapmaq ucun ');
+
+let duzbucaqliSahesi = a * b;
+document.getElementById('aspa').innerHTML =duzbucaqliSahesi;
+
+
+
+
+
+let x  = +prompt('perimetri bir terefi');
+let y = +prompt('diger terefi');
+
+let dustur =  2(x * y);
+document.getElementById('aspa').innerHTML =dustur;
